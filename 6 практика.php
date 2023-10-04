0848051c00767aa0030e49292bf5e3b52b6a00d2
sohib1337 <75015669+sohib1337@users.noreply.github.com>
2023-10-04 11:59:58 +0300

    Update 6 практика.php

--- 6 практика.php
+++ 6 практика.php
@@ -1,14 +1,19 @@
-   <html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <form action="1.php" method="post">
-            <p>Ваше имя: <input type="text" name="name"></p>
-            <p>Ваш возраст: <input type="text" name="age"></p>
-            <p><input type="submit"></p> 
-        </form>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="otv\2otvet.php" method="post">
+
+<p>Ваше имя: <input type="text" name="firstName" required></p>
+<p>Ваш возраст: <input type="number" name="twoName" min="0" required></p>
+<p><input type="submit"></p>
+
+
+
+</form>
+</body>
 </html>
