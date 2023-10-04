8d768450d57c0158813f658e6a005a34c65e61e7
sohib1337 <75015669+sohib1337@users.noreply.github.com>
2023-10-04 12:02:17 +0300

    пркатика 6.php

--- пркатика 6.php
+++ пркатика 6.php
@@ -1,13 +1,16 @@
-<?php 
-    if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
-        if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit;
-        
-        if(empty($_POST['name'])) exit('Поле "имя" не заполнено');
-        if(empty($_POST['age'])) exit('Поле "возраст" не заполнено');
-        if((int)$_POST['age'] < 1) exit('Ваш возраст введено отрицательное или НЕ числовое значение');
-        
-        echo 'Здравствуйте, '.htmlspecialchars($_POST['name']).'. <br>';
-        echo 'Вам '.(int)$_POST['age'].' лет.'.'<br>';
-    }
+<html>
+<body>
+ 
+<?php
+
+if( $_SERVER['REQUEST_METHOD'] !== 'POST' )
+    exit('Не POST');
+
+if(empty($_POST['twoName']) || empty($_POST['twoName'])) exit('Не все поля заполнены');
+if(!is_numeric($_POST['twoName']) || $_POST['twoName'] < 1) exit('Поле возраст заполнено неправильно');
+echo 'Здравствуйте, '.htmlspecialchars($_POST['firstName']).'. <br>';
+echo 'Вам '.(int)$_POST['twoName'].' лет.'.'<br>';
+
 ?>
-  
+</body>
+</html>
